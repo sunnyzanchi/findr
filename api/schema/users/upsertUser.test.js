63de2a501d4b5a7f271cdb0dd4e46d7aa82dd946
Zanchi <emzanchi@gmail.com>
2018-03-15 00:19:06 -0500

    Updated test to cover two cases I missed before

--- api/schema/users/upsertUser.test.js
+++ api/schema/users/upsertUser.test.js
@@ -22,6 +22,23 @@ test('should be able to add a user', async t => {
   t.true(user.username === userToAdd.username);
 });
 
+test('should be able to update a user', async t => {
+  const email = 'testing@example.com';
+  const query = `
+  mutation {
+    user(user: { id: 111, email: "${email}" }) {
+      id
+      email
+      username
+    }
+  }`;
+
+  const user = await gql(query);
+
+  t.true(user.id === '111');
+  t.true(user.email === email);
+});
+
 test('throws if no user is provided', async t => {
   const query = `
   mutation {
@@ -53,5 +70,17 @@ test('throws if no username is provided', async t => {
       username
     } 
   }`;
+  await t.throws(gql(query));
+});
+
+test('throws if trying to create a user with an existing username', async t => {
+  const query = `
+  mutation {
+    user(user: { password: "a", username: "a" }) {
+      id
+      username
+    }
+  }`;
+
   await t.throws(gql(query));
 });
