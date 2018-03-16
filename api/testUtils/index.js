e9fb5984ada3cacb5ec526af04bb1250928d3459
Zanchi <emzanchi@gmail.com>
2018-03-15 20:20:38 -0500

    #10 Updated test utils and auth, added password hashes for test users

--- api/testUtils/index.js
+++ api/testUtils/index.js
@@ -1,6 +1,12 @@
 const { graphql } = require('graphql');
+const { store } = require('../auth');
 const schema = require('../schema');
 
+/**
+ * @param {string} query The query to test against the GraphQL schema
+ * @param {object} variables Any variables to provide
+ * @param {object} context Optional context object to provide
+ */
 const gql = (query, variables, context) =>
   graphql(schema, query, undefined, context, variables)
     .then(result => {
@@ -11,6 +17,21 @@ const gql = (query, variables, context) =>
     })
     .then(data => Object.values(data)[0]);
 
+/**
+ * @return {object} An object with the same signature of the context that's passed by express
+ */
+const httpContext = () => {
+  const context = {
+    req: {},
+    res: {},
+  };
+
+  store.generate(context.req);
+  context.req.sessionStore = store;
+  return context;
+};
+
 module.exports = {
   gql,
+  httpContext,
 };
