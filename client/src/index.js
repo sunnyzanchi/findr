46fbebafad3962a5435e55a791c18bb6bb6b717f
Zanchi <emzanchi@gmail.com>
2020-04-18 18:56:01 -0700

    Updated with build:dev script, added LoginOrRegister page and useLocalStorage hook

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,27 @@
 import { h, render } from 'preact'
 import Router from 'preact-router'
-import Home from './pages/Home'
 
-const App = () => (
-  <Router>
-    <Home path="/" />
-  </Router>
-)
+import CreateList from './pages/CreateList'
+import Home from './pages/Home'
+import Lobby from './pages/Lobby'
+import LoginOrRegister from './pages/LoginOrRegister'
+import Register from './pages/Register'
+
+import useLocalStorage from './useLocalStorage'
+
+const App = () => {
+  // eslint-disable-next-line no-unused-vars
+  const [session, setSession] = useLocalStorage('session')
+
+  return (
+    <Router>
+      <LoginOrRegister path="/" />
+      <Home path="/app" />
+      <CreateList path="/create" />
+      <Lobby path="/lobby" />
+      <Register path="/register" />
+    </Router>
+  )
+}
 
 render(<App />, document.querySelector('#app'))
